80e703bdb8b88366c55ba9b0870807abf0805bc1
Kevin Buchik <kbuchik@gmail.com>
2021-04-20 19:48:15 -0700

    readme images

--- public/app.js
+++ public/app.js
@@ -1,18 +1,18 @@
 
-window.onload = function() {
-    getLocation();
+// window.onload = function() {
+//     getLocation();
     
-  };
+//   };
 
-  function getLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition);
-    } else { 
-      console.log("nope buddy try again");
-    }
-  }
+//   function getLocation() {
+//     if (navigator.geolocation) {
+//       navigator.geolocation.getCurrentPosition(showPosition);
+//     } else { 
+//       console.log("nope buddy try again");
+//     }
+//   }
   
-  function showPosition(position) {
-    console.log(position.coords.latitude)
-    console.log(position.coords.longitude);
-  }
+//   function showPosition(position) {
+//     console.log(position.coords.latitude)
+//     console.log(position.coords.longitude);
+//   }
